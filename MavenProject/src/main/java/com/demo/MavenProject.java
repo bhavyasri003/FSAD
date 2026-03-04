f429c6802cf5a9ffb69dc3f42f1a46921b8ceae4
bhavyasri003 <2420030181@klh.edu.in>
2026-03-04 20:09:00 +0530

    Add files via upload

---
+++ MavenProject/src/main/java/com/demo/MavenProject.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+public class MavenProject {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi this is my new marven project");
+
+	}
+
+}
